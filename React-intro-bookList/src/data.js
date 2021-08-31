32953fe5aff523ffe04cb8070512b627c4ec79de
dixith417 <66620655+dixith417@users.noreply.github.com>
2021-08-31 13:34:19 +0530

    Create data.js

---
+++ React-intro-bookList/src/data.js
@@ -0,0 +1,22 @@
+// vars and data
+export const bookDt = [
+  {
+    id: 1,
+    title: "The New Robin Sharma Pack",
+    author: "Robin Sharma",
+    img: "https://m.media-amazon.com/images/I/710nOH8xOnL._AC_UY218_.jpg",
+  },
+  {
+    id: 2,
+    title: "Write Me a Love Story",
+    author: "Ruskin Bond",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51S3Dg2WCBL._AC_SX184_.jpg",
+  },
+  {
+    id: 3,
+    title: "The Perfect Murder",
+    author: "Ruskin Bond",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51KG1LCYAtL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
+  },
+];
+// we can add more data to add more books...
